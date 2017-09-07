df52d8b3cafb120c1bf9d731c832936dc38a9610
csan21 <chrisswho@gmail.com>
2017-09-07 13:25:20 -0500

    Web portion complete, will add media queries for mobile

--- js/application.js
+++ js/application.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('.slide-show').chocolate({
     images: ['img/slide-room.jpg', 'img/slide-feet.jpg'],
     interval: 5000,
-    speed: 2500
+    speed: 5000
   });
 });
